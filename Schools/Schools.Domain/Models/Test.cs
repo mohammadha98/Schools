727ca2005f06630d3616940c2fd6a188b4264814
mohammadha98 <59112199+mohammadha98@users.noreply.github.com>
2020-08-03 11:41:50 +0430

    Added a Model For Test

---
+++ Schools/Schools.Domain/Models/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Schools.Domain.Models
+{
+   public class Test
+    {
+        public int Id { get; set; }
+
+        public string Sample { get; set; }
+    }
+}
