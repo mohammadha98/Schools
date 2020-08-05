63a24101d0a74d8a8ee7d7a60e75085a37b8ac9b
mohammadha98 <59112199+mohammadha98@users.noreply.github.com>
2020-08-05 13:00:16 +0430

    Users And Roles Models Added

---
+++ Schools/Schools.Domain/Models/Users/UserRole.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Schools.Domain.Models.Users
+{
+   public class UserRole
+    {
+        public UserRole()
+        {
+
+        }
+        [Key]
+        public int UserRoleId { get; set; }
+
+        public int UserId { get; set; }
+        public int RoleId { get; set; }
+
+
+        #region Relations
+
+        public virtual Users User { get; set; }
+        public virtual Role Role { get; set; }
+
+        #endregion
+    }
+}
---
+++ Schools/Schools.Domain/Models/Users/Users.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Schools.Domain.Models.Users
+{
+    public class Users
+    {
+
+        public Users()
+        {
+
+        }
+
+
+        [Key]
+        public int UserId { get; set; }
+
+
+        [Display(Name = "نام کاربری")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(100, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+
+        public string UserName { get; set; }
+
+        [Display(Name ="رمز عبور")]  
+        [MaxLength(50, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+
+        public string Password { get; set; }
+
+
+
+        [Display(Name = "کد فعال سازی")]
+        [MaxLength(50, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string ActiveCode { get; set; }
+
+        [Display(Name = "وضعیت")]
+        public bool IsActive { get; set; }
+
+        [Display(Name = "آواتار")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string UserAvatar { get; set; }
+
+        [Display(Name = "تاریخ ثبت نام")]
+        public DateTime RegisterDate { get; set; }
+
+        public bool IsDelete { get; set; }
+
+
+
+        #region Relations
+
+        public virtual List<UserRole> UserRoles { get; set; }
+
+        #endregion
+    }
+}
