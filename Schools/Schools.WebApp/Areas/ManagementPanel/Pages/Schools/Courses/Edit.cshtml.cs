e0042b19ea79054579a37deeddb0bc7057b3f241
mohammad-ashrafi80 <m.a.mohammad.ashrafi@gmail.com>
2020-08-07 15:51:19 +0430

    Build ManageMentPanel

---
+++ Schools/Schools.WebApp/Areas/ManagementPanel/Pages/Schools/Courses/Edit.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Schools.WebApp.Areas.ManagementPanel.Pages.Schools.Courses
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
