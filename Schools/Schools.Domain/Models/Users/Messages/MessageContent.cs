3b6ce817875f9cfdc6cbdb6e2ec90a05bf6cd2d9
mohammad-ashrafi80 <m.a.mohammad.ashrafi@gmail.com>
2020-08-07 11:54:49 +0430

    Create  User-Ticket-Notification-Messages

---
+++ Schools/Schools.Domain/Models/Users/Messages/MessageContent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Schools.Domain.Models.Users.Messages
+{
+    public class MessageContent
+    {
+        [Key]
+        public int ContentId { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public int MessageId { get; set; }
+        [Required(ErrorMessage = "متن پیام را وارد کنید")]
+        public string Text { get; set; }
+        public DateTime SendDate { get; set; }
+        public bool IsSeen { get; set; }
+        public bool IsDelete { get; set; }
+
+        #region Relations
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+        [ForeignKey("MessageId")]
+        public UserMessage UserMessage { get; set; }
+
+        #endregion
+
+    }
+}
---
+++ Schools/Schools.Domain/Models/Users/UserNotification.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Schools.Domain.Models.Users
+{
+    public class UserNotification
+    {
+        [Key]
+        public int NotificationId { get; set; }
+        public int UserId { get; set; }
+        [Required(ErrorMessage = "عنوان پیغام را وارد کنید")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "متن پیغام را وارد کنید")]
+        public string Text { get; set; }
+        [Required]
+        public bool IsSee { get; set; }
+        [Required]
+        public bool IsDelete { get; set; }
+        public DateTime CreateDate { get; set; }
+
+
+        #region Relations
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+        #endregion
+    }
+}
