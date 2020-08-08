ea4a07736467e9ef8b2b22d07b223088888f7261
mohammad-ashrafi80 <m.a.mohammad.ashrafi@gmail.com>
2020-08-08 15:48:18 +0430

    Add Migration And Update School Model

---
+++ Schools/Schools.Infra.Data/Migrations/20200808111432_Fix2_School.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Schools.Infra.Data.Migrations
+{
+    public partial class Fix2_School : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MetaDescription",
+                table: "Schools",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MetaDescription",
+                table: "Schools");
+        }
+    }
+}
