bc321ed0edcdd0e36a402b26f0a9aabdf50bb4d1
sepehrzarei <s.zareiweb@gmail.com>
2020-08-13 00:34:33 +0430

    Fixed Error for  Add User

--- Schools/Schools.Application/ViewModels/UsersViewModel/UsersForAdminPanelViewModel.cs
+++ Schools/Schools.Application/ViewModels/UsersViewModel/UsersForAdminPanelViewModel.cs
@@ -28,7 +28,6 @@ namespace Schools.Application.ViewModels.UsersViewModel
         public string Family { get; set; }        
         public string UserName { get; set; }
         public string Password { get; set; }
-        public List<int> SelectedRoles { get; set; }
-        public IFormFile Image { get; set; }
+        
     }
 }
--- Schools/Schools.WebApp/Areas/ManagementPanel/Pages/Users/Add.cshtml.cs
+++ Schools/Schools.WebApp/Areas/ManagementPanel/Pages/Users/Add.cshtml.cs
@@ -11,6 +11,7 @@ using Schools.Domain.Repository.InterfaceRepository.Users;
 using Schools.Domain.Models.Users;
 using Schools.Application.Utilities.Security;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
 
 namespace Schools.WebApp.Areas.ManagementPanel.Pages.Users
 {
@@ -22,45 +23,50 @@ namespace Schools.WebApp.Areas.ManagementPanel.Pages.Users
             _userRepository = userRepository;
         }
 
-        public User UserAdd { get; set; }
+        
         public List<Role> Roles { get; set; }
         [BindProperty]
         public AddUserViewModel add { get; set; }
+
         public void OnGet()
         {
             Roles = _userRepository.GetAllRoles();
         }
 
-        public IActionResult OnPost(List<int> SelectedRoles)
+        public IActionResult OnPost(List<int> SelectedRoles,IFormFile file)
         {
             if (!ModelState.IsValid)
                 return Page();
 
-            if (add.Image.FileName != "" || add.Image.FileName != null)
+
+            if (file.FileName != "" || file.FileName != null)
             {
-                if (add.Image.ContentType.EndsWith("jpg") || add.Image.ContentType.EndsWith("ong"))
+                if (file.FileName.EndsWith(".jpg") || file.FileName.EndsWith(".png"))
                 {
-                    var path = Path.Combine(Directory.GetCurrentDirectory(), "www/UserProfile", add.Image.FileName + Guid.NewGuid().ToString());
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/UserProfile", file.FileName + Guid.NewGuid().ToString());
                     using (var fs = new FileStream(path, FileMode.Create))
                     {
-                        add.Image.CopyTo(fs);
+                        file.CopyTo(fs);
                     }
                 }
             }
 
-            UserAdd.UserName = add.UserName;
-            UserAdd.Name = add.Name;
-            UserAdd.RegisterDate = DateTime.Now;
-            UserAdd.IsDelete = false;
-            UserAdd.Family = add.Family;
-            UserAdd.UserAvatar = add.Image.FileName;
-            UserAdd.IsActive = true;
-            UserAdd.Password = PasswordHelper.EncodePasswordMd5(add.Password);
-
-            var userId = _userRepository.AddUser(UserAdd);
+            User user = new User() 
+            { 
+                Family = add.Family,
+                Name = add.Name,
+                RegisterDate = DateTime.Now,
+                IsDelete = false,
+                UserName = add.UserName,
+                UserAvatar = file.FileName,
+                IsActive = true,
+                Password = PasswordHelper.EncodePasswordMd5(add.Password)
+            };
+            
+            var userId = _userRepository.AddUser(user);
             _userRepository.AddRolesForUser(SelectedRoles, userId);
 
-            return Page();
+            return RedirectToPage("Index");
         }
     }
 }
