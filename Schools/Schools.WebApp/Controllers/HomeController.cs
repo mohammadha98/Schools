e5288bc1c79c1c0f529420496f4f17232dc0c3b7
ali joozdani <ali110jozdani@hotmail.com>
2020-09-05 09:58:55 +0430

    fix

--- Schools/Schools.WebApp/Controllers/HomeController.cs
+++ Schools/Schools.WebApp/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Schools.Application.Service.Interfaces.Locations;
 using Schools.Application.Service.Interfaces.Schools;
+using Schools.Domain.Models.AboutUs;
 using Schools.Domain.Repository.InterfaceRepository.Locations;
 
 namespace Schools.WebApp.Controllers
@@ -10,12 +11,18 @@ namespace Schools.WebApp.Controllers
         private ILocationRepository _location;
         private ISchoolService _school;
         private ILocationService _locationService;
+        private IContactUsRepository _contactUs;
+        private IAboutUsRepository _aboutUs;
+        private IContactUsFormRepository _contactUsForm;
 
-        public HomeController(ILocationRepository location, ISchoolService school, ILocationService locationService)
+        public HomeController(ILocationRepository location, ISchoolService school, ILocationService locationService, IContactUsRepository contactUs, IAboutUsRepository aboutUs, IContactUsFormRepository contactUsForm)
         {
             _location = location;
             _school = school;
             _locationService = locationService;
+            _contactUs = contactUs;
+            _aboutUs = aboutUs;
+            _contactUsForm = contactUsForm;
         }
 
 
@@ -70,6 +77,29 @@ namespace Schools.WebApp.Controllers
                 result += $"<option value='{city.CityId}'>{city.CityTitle}</option>";
             }
             return result;
+        }
+
+        [Route("/AboutUs")]
+        public IActionResult AboutUs(AboutUs aboutUs)
+        {
+            var aboutus = _aboutUs.GetLast();
+            return View(aboutus);
+        }
+        [Route("/ContactUs")]
+        public IActionResult ContactUs()
+        {
+            return View(_contactUs.GetLast());
+        }
+        [HttpPost]
+        [Route("/ContactUs")]
+        public IActionResult ContactUs(ContactUsForm contactUsForm)
+        {
+            if (!ModelState.IsValid)
+                return View(_contactUs.GetLast());
+
+            _contactUsForm.InsertQuestion(contactUsForm);
+            ViewData["IsSuccess"] = true;
+            return View(_contactUs.GetLast());
         }
     }
 }
